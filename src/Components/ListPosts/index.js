d89473be8decc8565fe72c8c6b5fd72e202be569
flipggs <flipggs@gmail.com>
2018-03-05 22:33:19 -0300

    Feat: order POSTS  by voteScore and date

--- src/Components/ListPosts/index.js
+++ src/Components/ListPosts/index.js
@@ -1,21 +1,72 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Post from './../Post'
 
-const ListPosts = (props) => {
-    const { posts } = props || []
+class ListPosts extends Component {
 
-    return (
-        <div>
+    constructor(props) {
+        super(props)
+        this.state = {
+            posts: []
+        }
+    }
 
-            {posts.length > 0 && posts.map(post => (
-                <Post key={post.id}
-                    postData={post} />
-            ))}
+    componentWillReceiveProps(nextProps) {
+        if (this.state.posts.length === 0) {
+            const { posts } = nextProps || []
+            this.setState({posts: posts})
+            this.orderBy('timestamp')
+        }
+    }
 
-            {posts.length === 0 && (<h3>Nenhum post encontrado</h3>)}
+    orderBy = (field) => {
+        let { posts } = this.state
+        posts = posts.sort((a, b) => {
+            if (a[field] < b[field])
+                return -1;
+            if (a[field] > b[field])
+                return 1;
+            return 0;
+        })
 
-        </div>
-    )
+        this.setState(posts)
+    }
+
+    change = (event) => {
+        const { value } = event.target
+        this.orderBy(value)
+    }
+
+    render() {
+
+        const { posts } = this.state
+
+        return (
+            <div>
+
+                <div>
+                    <label>
+                        Ordenar por:
+                    </label>
+                    <select onChange={this.change}>
+                        <option value="timestamp">
+                            Data
+                        </option>
+                        <option value="voteScore">
+                            Votação
+                        </option>
+                    </select>
+                </div>
+
+                {posts.length > 0 && posts.map(post => (
+                    <Post key={post.id}
+                        postData={post} />
+                ))}
+
+                {posts.length === 0 && (<h3>Nenhum post encontrado</h3>)}
+
+            </div>
+        )
+    }
 }
 
 export default ListPosts
